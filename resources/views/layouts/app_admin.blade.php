ec2f449e9aa014d7a795d8042a858761313fdb24
sumiyatiannaj <sumiyatiannajmu@gmail.com>
2021-03-15 11:56:11 +0700

    setting icon tittle

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SKKS Krakatau Steel</title>
   <!-- Tell the browser to be responsive to screen width -->
+<link rel="shortcut icon" href="https://sso.krakatausteel.com/public/fav.png">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="{{url(link_html().'/bower_components/bootstrap/dist/css/bootstrap.min.css')}}">
--- resources/views/layouts/app_admin_kanan.blade.php
+++ resources/views/layouts/app_admin_kanan.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SKKS Krakatau Steel</title>
+  <link rel="shortcut icon" href="https://sso.krakatausteel.com/public/fav.png">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
